41fe9c3dc4d59ca3d4f07b76170af69d63207ad5
Toto Briac <totobriac@gmail.com>
2020-10-13 20:10:57 +0200

    PEP8 Done

--- trusty_monkey/users/api/views.py
+++ trusty_monkey/users/api/views.py
@@ -2,8 +2,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from users.api.serializers import UserDisplaySerializer
 
+
 class CurrentUserAPIView(APIView):
 
     def get(self, request):
-        serializer= UserDisplaySerializer(request.user)
+        serializer = UserDisplaySerializer(request.user)
         return Response(serializer.data)
